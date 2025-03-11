b85cd02c2a4d9e9d1319c386192cf096bf843c44
suryansh1619 <suryanshsahu2812@gmail.com>
2025-03-11 17:17:31 +0530

    update the backend url

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -1,2 +1,3 @@
-const API_BASE_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:5000";
+require("dotenv").config();
+const API_BASE_URL = process.env.VITE_BACKEND_URL || "http://localhost:5000";
 export default API_BASE_URL;
